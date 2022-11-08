1bd33aaa42b2624b76551c68147e48afe3773d14
EXTRADEEN <dinumira@gmail.com>
2022-11-08 00:55:20 -0800

    main.h update

--- main.h
+++ main.h
@@ -16,9 +16,7 @@ typedef struct s_format
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_i(va_list i);
-int print_d(va_list d);
+int print_char(va_list args);
+int print_string(va_list args);
 
 #endif /* _MAIN_H */
