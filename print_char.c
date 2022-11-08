2518e3b44b1d81161c675406b7f42b8c53fb5b97
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-11-08 01:17:01 -0800

    update

--- print_char.c
+++ print_char.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_c - A function that prints a charactar
- * @c: character to print
+ * print_char - A function that prints a charactar
+ * @args: character to print
  * Return: Always 1 (Succes)
  */
 int print_char(va_list args)
 {
 	char character;
-	
+
 	character = va_arg(args, int);
 
 	_putchar(character);
