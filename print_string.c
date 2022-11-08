1e6b91fb179d64130f6cb1a835d9fa3ca7b2f665
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-11-08 00:41:47 -0800

    update

--- print_string.c
+++ print_string.c
@@ -2,18 +2,22 @@
 #include <unistd.h>
 
 /**
- * print_s - A function that prints a string
- * @s: string to print
- * Return: Always 1 (Succes)
+ * print_string - A function that prints a string.
+ * @s: string to print.
+ * Return: Always 1 (Success)
  */
-int print_s(va_list s)
+int print_string(va_list args)
 {
-	char *str = va_arg(s, char *);
+	char *str = va_arg(args, char *);
 	int i = 0;
 
 	if (str == NULL)
 		str = "(null)";
+
 	while (str[i])
-		_putchar(str[i++]);
+	{
+		_putchar(str[i]);
+		i++
+	}
 	return (i);
 }
